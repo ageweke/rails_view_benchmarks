584bd91e8b650a516a89d93cb8a120e3d28ef286
Andrew Geweke <andrew@geweke.org>
2014-01-12 17:48:51 -0800

    Much more factoring of the Place Page ERb view.

--- app/controllers/benchmark_controller.rb
+++ app/controllers/benchmark_controller.rb
@@ -6,7 +6,31 @@ class BenchmarkController < ApplicationController
   def benchmark_place_page
     @place = "San Francisco"
 
-    @user = OpenStruct.new(:name => 'Andrew Geweke', :id => 84390252)
+    @user = OpenStruct.new(
+      :name => 'Joe Smith',
+      :id => 84390252,
+      :profile_image => "/n/image/main_image/1/84390252/1389477462/C-60-60?default=true&amp;sh=cDC5YUsZOQLnQnLQ39ahyA",
+      :unread_messages => 57,
+      :first_name => 'Joe',
+      :profile_path => '/profile.html?id=890FZQ')
+
+    @subnav_places = [
+      OpenStruct.new(:link => "/n/places/san-francisco-california-united-states", :title => "San Francisco Bay", :followed => true),
+      OpenStruct.new(:link => "/n/places/yosemite-national-park-california-united-states", :title => "Yosemite National Park", :followed => true),
+      OpenStruct.new(:link => "/n/places/rocky-mountain-national-park-united-states", :title => "Rocky Mountain National Park", :followed => true),
+      OpenStruct.new(:link => "/n/places/barcelona-cataluna-spain", :title => "Barcelona"),
+      OpenStruct.new(:link => "/n/places/berlin-berlin-germany", :title => "Berlin"),
+      OpenStruct.new(:link => "/n/places/boston-massachusetts-united-states", :title => "Boston"),
+      OpenStruct.new(:link => "/n/places/budapest-budapest-hungary", :title => "Budapest"),
+      OpenStruct.new(:link => "/n/places/buenos-aires-autonomous-city-of-buenos-aires-argentina", :title => "Buenos Aires"),
+      OpenStruct.new(:link => "/n/places/cairo-cairo-governorate-egypt", :title => "Cairo"),
+      OpenStruct.new(:link => "/n/places/cape-town-western-cape-south-africa", :title => "Cape Town"),
+      OpenStruct.new(:link => "/n/places/istanbul-turkey", :title => "Istanbul"),
+      OpenStruct.new(:link => "/n/places/melbourne-victoria-australia", :title => "Melbourne"),
+      OpenStruct.new(:link => "/n/places/mexico-city-distrito-federal-mexico", :title => "Mexico City"),
+      OpenStruct.new(:link => "/n/places/montreal-quebec-canada", :title => "Montreal"),
+      OpenStruct.new(:link => "/n/places/nairobi-nairobi-kenya", :title => "Nairobi")
+    ]
 
     benchmark!({ })
   end
--- app/helpers/erb_place_page_helper.rb
+++ app/helpers/erb_place_page_helper.rb
@@ -2,4 +2,8 @@ module ErbPlacePageHelper
   def cdn(x)
     "https://d20rrv4k3o7n66.cloudfront.net" + x
   end
+
+  def s3(x)
+    "http://files.couchsurfing.s3.amazonaws.com" + x
+  end
 end
