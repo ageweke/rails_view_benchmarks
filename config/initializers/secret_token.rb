214f423e3dd07f855be685c2adcdc276bc0407a9
Andrew Geweke <andrew@geweke.org>
2014-01-12 16:28:29 -0800

    Initial Rails project for Rails view benchmarks.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsViewBenchmarks::Application.config.secret_key_base = 'ff4dec2387b62d1c1df6a9edf42babb040fdd3f620674437ef4392b4cb9eab90257e82ba0eb7166b8e28646059c3f2cb79903baeed88dbbbf17a2f88ff2e40fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsViewBenchmarks::Application.config.session_store :cookie_store, key: '_rails_view_benchmarks_session'
